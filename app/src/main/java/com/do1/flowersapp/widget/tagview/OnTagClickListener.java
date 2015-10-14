fa639e5325599faee2fac8d9471b416ae5a3428b
brucetoo <brucetoo14@gmail.com>
2015-10-14 21:20:25 +0800

    添加tagview控件

---
+++ app/src/main/java/com/do1/flowersapp/widget/tagview/OnTagClickListener.java
@@ -0,0 +1,8 @@
+package com.do1.flowersapp.widget.tagview;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagClickListener {
+	void onTagClick(Tag tag, int position);
+}
---
+++ app/src/main/java/com/do1/flowersapp/widget/tagview/OnTagDeleteListener.java
@@ -0,0 +1,8 @@
+package com.do1.flowersapp.widget.tagview;
+
+/**
+ * listener for tag delete
+ */
+public interface OnTagDeleteListener {
+	void onTagDeleted(Tag tag, int position);
+}
