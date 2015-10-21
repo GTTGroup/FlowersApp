9582864644f77303a88c6da1078f777662237fe3
brucetoo <brucetoo14@gmail.com>
2015-10-21 23:51:22 +0800

    商家详情-商家首页tab UI

--- app/src/main/java/com/do1/flowersapp/fragment/SellerDetailHomeFragment.java
+++ app/src/main/java/com/do1/flowersapp/fragment/SellerDetailHomeFragment.java
@@ -3,10 +3,15 @@ package com.do1.flowersapp.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+
+import com.do1.flowersapp.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by Bruce Too
@@ -14,11 +19,40 @@ import android.widget.TextView;
  * At 21:24
  */
 public class SellerDetailHomeFragment extends Fragment {
+
+    @Bind(R.id.recycler_detail_main)
+    RecyclerView mRecylerView;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        TextView view = new TextView(container.getContext());
-        view.setText("SellerDetailHomeFragment");
+        View view = inflater.inflate(R.layout.fragment_seller_detail_home,container,false);
+        ButterKnife.bind(this,view);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+
+    class HomeRecyclerAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
+
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
