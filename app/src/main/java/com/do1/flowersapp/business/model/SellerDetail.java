43cbb32f736cd28cbc6bd29c6d17d2e7ca6be3a2
brucetoo <brucetoo14@gmail.com>
2015-10-22 16:03:05 +0800

    商家详情主tab 界面模拟数据跑通

---
+++ app/src/main/java/com/do1/flowersapp/business/model/SellerDetail.java
@@ -0,0 +1,26 @@
+package com.do1.flowersapp.business.model;
+
+import com.google.gson.JsonElement;
+
+/**
+ * Created by TuYong N1007
+ * On 2015/10/22
+ * At 14:32
+ * 商家详情信息
+ */
+public class SellerDetail {
+//    code	返回结果	String
+//    desc	结果描述	String
+//    data	返回实体
+//    id	商铺ID	String
+//    shopName	商铺名称	String
+//    shopInfoUrl	商家简介地址	String
+//    addr	商家介绍地址	String
+//    linkService	联系方式	String
+     public String id;
+     public String shopName;
+     public String shopInfoUrl;
+     public String addr;
+     public String linkService;
+     public JsonElement data;
+}
